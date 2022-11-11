f054a4bebe75287525eb49d30b798032df45b0d0
apmfree78 <amit@profitswami.com>
2022-11-11 14:40:39 -0500

    setup husky pre-commit hooks with lint-staged and pre-push hooks with npm test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import 'bulmaswatch/slate/bulmaswatch.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './App.css';
-import MintInfo from './components/MintInfo';
+import React from "react";
+import "bulmaswatch/slate/bulmaswatch.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./App.css";
+import MintInfo from "./components/MintInfo";
 
 function App() {
-  return <div className='App'>
-    <MintInfo />
-  </div>;
+  return (
+    <div role="container" className="App">
+      <MintInfo />
+    </div>
+  );
 }
 
 export default App;
