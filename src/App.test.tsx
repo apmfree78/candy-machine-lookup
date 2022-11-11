39d610d1aca6494e5084d18159199d0438e70bc1
apmfree78 <amit@profitswami.com>
2022-11-11 17:34:20 -0500

    mock axios in App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+jest.mock("axios", () => () => null);
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByRole("container");
