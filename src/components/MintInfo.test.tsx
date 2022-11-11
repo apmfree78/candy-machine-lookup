23a0adafd8967f69f7e13da9b4cc81d5cbbd25c9
apmfree78 <amit@profitswami.com>
2022-11-11 15:22:40 -0500

    add tables to MintInfo component

--- src/components/MintInfo.test.tsx
+++ src/components/MintInfo.test.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import MintInfo from './MintInfo';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import MintInfo from "./MintInfo";
 
-test('Mint Price Column Appears', () => {
+test("Mint Price Column Appears", () => {
   render(<MintInfo />);
   const Element = screen.getByText(/mint price/i);
   expect(Element).toBeInTheDocument();
 });
 
-test('Mint Stats Column Appears', () => {
+test("Live Date Appears", () => {
+  render(<MintInfo />);
+  const Element = screen.getByText(/live date/i);
+  expect(Element).toBeInTheDocument();
+});
+
+test("Mint Stats Column Appears", () => {
   render(<MintInfo />);
   const Element = screen.getByText(/mint stats/i);
   expect(Element).toBeInTheDocument();
 });
 
-test('Creators Column Appears', () => {
+test("Creators Column Appears", () => {
   render(<MintInfo />);
   const Element = screen.getByText(/creators/i);
   expect(Element).toBeInTheDocument();
--- src/components/MintInfo.tsx
+++ src/components/MintInfo.tsx
@@ -1,17 +1,65 @@
 // component to print out general info on specific Candy Machine
 const MintInfo: React.FC = () => {
   return (
-    <section aria-label='candy-machine-data' className='columns'>
-      <div className='column'>
-        <h5 className='title is-5'>Mint Price</h5>
+    <section aria-label="candy-machine-data" className="columns">
+      <div className="column" style={{ textAlign: "left" }}>
+        <p>
+          <p style={{ fontWeight: "bold" }}>Mint Price</p>
+          <p>3 SOL</p>
+        </p>
+        <br />
+        <p>
+          <p style={{ fontWeight: "bold" }}>Live Date</p>
+          <p>Febrary 2, 2022</p>
+          <p>6 hours ago</p>
+        </p>
       </div>
-      <div className='column'>
-
-        <h5 className='title is-5'>Mint Stats</h5>
+      <div className="column" style={{ textAlign: "left" }}>
+        <h5 className="title is-5">Mint Stats</h5>
+        <table className="table">
+          <tbody>
+            <tr>
+              <td>Items Avaliable</td>
+              <td>10000</td>
+            </tr>
+            <tr>
+              <td>Items Redeemed</td>
+              <td>7422/10000</td>
+            </tr>
+            <tr>
+              <td>Items Remaining</td>
+              <td>2578/10000</td>
+            </tr>
+            <tr>
+              <td>Royalties</td>
+              <td>5%</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <div className='column'>
+      <div className="column" style={{ textAlign: "left" }}>
+        <h5 className="title is-5">Creators</h5>
 
-        <h5 className='title is-5'>Creators</h5>
+        <table className="table">
+          <tbody>
+            <tr>
+              <td>J9wb...WG43</td>
+              <td>25%</td>
+            </tr>
+            <tr>
+              <td>8wea...WcNJ</td>
+              <td>25%</td>
+            </tr>
+            <tr>
+              <td>6vPMH...wbvU</td>
+              <td>25%</td>
+            </tr>
+            <tr>
+              <td>DpZJ...xGct</td>
+              <td>25%</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </section>
   );
